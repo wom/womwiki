b932c862f35a1f0eaf642101a632ad6bc59a709f
Chris <wom@users.noreply.github.com>
2026-01-31 23:33:18 -0500

    feat: simplify capture keymaps by removing redundant mapping
    Remove the separate <leader>wQ mapping and consolidate capture
    functionality under <leader>wq for both normal and visual modes.
    This reduces keymap complexity while maintaining all capture
    features.
    
    - Remove womwiki.capture mapping
    - Change <leader>wq to use capture_with_location in normal mode
    - Update visual mode description to remove "+ location" text

--- plugin/womwiki.lua
+++ plugin/womwiki.lua
@@ -11,8 +11,7 @@ if not vim.g.womwiki_disable_mappings then
 	vim.keymap.set({ "n", "v" }, "<leader>w", womwiki.picker, { desc = "womwiki!" })
 	vim.keymap.set("n", "<leader>wb", womwiki.backlinks, { desc = "womwiki backlinks" })
 	vim.keymap.set("n", "<leader>wg", womwiki.show_graph, { desc = "womwiki graph view" })
-	vim.keymap.set("n", "<leader>wq", womwiki.capture, { desc = "womwiki quick capture" })
-	vim.keymap.set("n", "<leader>wQ", womwiki.capture_with_location, { desc = "womwiki capture + location" })
-	vim.keymap.set("v", "<leader>wq", womwiki.capture_visual, { desc = "womwiki capture selection + location" })
+	vim.keymap.set("n", "<leader>wq", womwiki.capture_with_location, { desc = "womwiki quick capture" })
+	vim.keymap.set("v", "<leader>wq", womwiki.capture_visual, { desc = "womwiki capture selection" })
 	vim.keymap.set("n", "<leader>wi", womwiki.inbox, { desc = "womwiki inbox" })
 end
